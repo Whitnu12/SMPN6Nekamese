a4c99740fde08fdd8ac08a4d25a3985f4fec6b2e
Dzikril <daengdziha77@gmail.com>
2022-08-14 02:47:15 +0700

    test sejarah

---
+++ application/views/depan/v_sejarah.php
@@ -0,0 +1,7 @@
+<div class="event_details_info">
+                            <div class="event_info">
+                                    <h4>VISI MISI SEKOLAH</h4>
+                                <p><span> <i class="flaticon-clock"></i> 10:30 pm</span> <span> <i class="flaticon-calendar"></i> 21 Nov 2020 </span> <span> <i class="flaticon-placeholder"></i> AH Oditoriam</span> </p>
+                            </div>
+
+                        </div>
