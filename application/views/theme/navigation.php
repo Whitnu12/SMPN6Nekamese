1d9eed7ebce042ee2ee7bdfc1dc1ad1c1192b96a
Muhammad hasan basri <72975517+hsanbsri@users.noreply.github.com>
2022-08-18 10:31:36 +0700

    update content anda spacing

--- application/views/theme/navigation.php
+++ application/views/theme/navigation.php
@@ -1,47 +1,47 @@
 <div id="sticky-header" class="main-header-area">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="header_wrap d-flex justify-content-between align-items-center">
-                                <div class="header_left">
-                                    <div class="logo">
-                                        <a href="<?php echo base_url('');?>">
-                                            <img src="<?php echo base_url('');?>style/img/sekolah.png" alt="">
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="header_right d-flex align-items-center">
-                                    <div class="main-menu  d-none d-lg-block">
-                                        <nav>
-                                            <ul id="navigation">
-                                                <li><a  href="<?php echo base_url('');?>">home</a></li>                                                
-                                                <li><a href="#">Profile <i class="ti-angle-down"></i></a>
-                                                    <ul class="submenu">
-                                                        <li><a href="<?php echo site_url('about');?>">Sambutan</a></li>
-                                                        <li><a href="<?php echo site_url('sejarah');?>">Sejarah</a></li>
-                                                        <li><a href="<?php echo site_url('profil');?>">Tentang</a></li>
-                                                        <li><a href="<?php echo site_url('visi_misi');?>">Visi Misi</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="#">Akademik <i class="ti-angle-down"></i></a>
-                                                    <ul class="submenu">
-                                                        <li><a href="<?php echo site_url('pengumuman');?>">Pengumuman</a></li>
-                                                        
-                                                        <li><a href="<?php echo site_url('agenda');?>">Agenda</a></li>
-                                                        <li><a href="<?php echo site_url('galeri');?>">Galeri</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li><a href="<?php echo site_url('blog');?>">Kegiatan</a></li>
-                                            </ul>
-                                        </nav>
-                                    </div>
-                                    
-                                </div>
-                            </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="header_wrap d-flex justify-content-between align-items-center">
+                    <div class="header_left">
+                        <div class="logo">
+                            <a href="<?php echo base_url(''); ?>">
+                                <img src="<?php echo base_url(''); ?>style/img/sekolah.png" alt="">
+                            </a>
                         </div>
-                        <div class="col-12">
-                            <div class="mobile_menu d-block d-lg-none"></div>
+                    </div>
+                    <div class="header_right d-flex align-items-center">
+                        <div class="main-menu  d-none d-lg-block">
+                            <nav>
+                                <ul id="navigation">
+                                    <li><a href="<?php echo base_url(''); ?>">home</a></li>
+                                    <li><a href="#">Profile <i class="ti-angle-down"></i></a>
+                                        <ul class="submenu">
+                                            <li><a href="<?php echo site_url('about'); ?>">Sambutan</a></li>
+                                            <li><a href="<?php echo site_url('sejarah'); ?>">Sejarah</a></li>
+                                            <li><a href="<?php echo site_url('profil'); ?>">Tentang</a></li>
+                                            <li><a href="<?php echo site_url('visi_misi'); ?>">Visi Misi</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Akademik <i class="ti-angle-down"></i></a>
+                                        <ul class="submenu">
+                                            <li><a href="<?php echo site_url('pengumuman'); ?>">Pengumuman</a></li>
+
+                                            <li><a href="<?php echo site_url('agenda'); ?>">Agenda</a></li>
+                                            <li><a href="<?php echo site_url('galeri'); ?>">Galeri</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="<?php echo site_url('blog'); ?>">Kegiatan</a></li>
+                                </ul>
+                            </nav>
                         </div>
+
                     </div>
                 </div>
             </div>
+            <div class="col-12">
+                <div class="mobile_menu d-block d-lg-none"></div>
+            </div>
+        </div>
+    </div>
+</div>
